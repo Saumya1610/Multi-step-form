985f30da87e6ffa3f48158d3a6c47e653ca17e1a
SAUMYA <saumyaagrawal106@gmail.com>
2024-04-08 00:35:42 +0530

    basic impementation of multi step form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React from "react";
-import Header from "./components/Header";
-import Option from "./components/Option";
-import Button from "./components/Button";
-
+import About from "./pages/About";
 
 function App() {
   return (
-    <div className="p-8">
-    <div className="flex gap-2 items-center">
-    <ion-icon name="chevron-back-outline"></ion-icon>
-    <progress className="w-[90%] h-1 flex justify-center items-center" value={0.2} />
-    </div>
-   <Header/>
-   <div className="flex flex-col gap-3">
-    <Option/>
-    <Option/>
-    <Option/>
-    <Option/>
-    <Option/>
-   </div>
-   <Button/>
+    <div className="p-4">
+      <div className="flex gap-2 items-center">
+        <ion-icon name="chevron-back-outline"></ion-icon>
+        <progress
+          className="w-[90%] h-1 flex justify-center items-center"
+          value={0.2}
+        />
+      </div>
+      <About/>
     </div>
   );
 }
---
+++ src/pages/Interest.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import Option from "../components/Option";
+import Button from "../components/Button";
+
+export default function About() {
+  return (
+    <>
+      <Header />
+      <div className="flex flex-col gap-3">
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+        <Option />
+      </div>
+      <Button />
+    </>
+  );
+}
